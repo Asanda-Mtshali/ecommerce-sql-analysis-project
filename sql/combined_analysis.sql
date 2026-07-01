376989455d6aef91f170cb96f6e820c125d1d1de
Asanda Sinenhlanhla Mtshali <mtshaliasanda169@gmail.com>
2026-07-01 09:10:39 +0200

    combined_analysis.sql

--- sql/combined_analysis.sql
+++ sql/combined_analysis.sql
@@ -7,3 +7,10 @@ JOIN order_payment_dataset p
     ON o.order_id = p.order_id
 JOIN order_items oi 
     ON o.order_id = oi.order_id;
+
+## 📌 Final Business Recommendations
+
+- Focus on improving logistics due to freight cost impact
+- Encourage repeat customers through loyalty programs
+- Optimize seller performance distribution
+- Promote alternative payment methods to reduce dependency on credit cards
